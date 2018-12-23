9eae28bf52c1d8512a5d483f7d303c1a36ccd632
Sangwon Ryu <yottabyte090@naver.com>
2018-12-23 17:35:07 +0900

    이벤트 핸들링과 관련된 실험적인 기능 제거

--- Dust/Dust.cs
+++ Dust/Dust.cs
@@ -15,12 +15,6 @@ namespace Dust
 
         static void Main(string[] args)
         {
-            window.Closed += Event_Closed;
-            window.TextEntered += Event_TextEntered;
-            window.MouseMoved += Event_MouseMoved;
-            window.MouseButtonPressed += Event_MouseButtonPressed;
-            window.MouseButtonReleased += Event_MouseButtonReleased;
-
             window.SetVerticalSyncEnabled(true);
             window.SetFramerateLimit(60);
 
@@ -40,32 +34,6 @@ namespace Dust
 
                 window.Display();
             }
-        }
-
-        static void Event_Closed(object sender, EventArgs e)
-        {
-            window.Close();
-        }
-
-        static void Event_TextEntered(object sender, TextEventArgs e)
-        {
-            
-        }
-
-        static void Event_MouseMoved(object sender, MouseMoveEventArgs e)
-        {
-            x = e.X;
-            y = e.Y;
-        }
-
-        static void Event_MouseButtonPressed(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
-        static void Event_MouseButtonReleased(object sender, MouseButtonEventArgs e)
-        {
-
         }
     }
 }
