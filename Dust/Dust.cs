fa1d3f667b00e35a56e57b4887f9409537ea3a0b
Sangwon Ryu <yottabyte090@naver.com>
2018-12-23 17:31:58 +0900

    실험적인 기능 삭제

--- Dust/Dust.cs
+++ Dust/Dust.cs
@@ -5,20 +5,11 @@ using Dust.Engine;
 
 namespace Dust
 {
-    enum PixelType
-    {
-        None,
-        Powder,
-        Block
-    }
 
     class Dust
     {
         private static RenderWindow window = new RenderWindow(new VideoMode(1024, 576), "Dust", Styles.Titlebar | Styles.Close);
-        private static Scene currentScene = new MainScene();
-        public static PixelType[,] data = new PixelType[1024, 576];
-
-        private static PixelType drawState = PixelType.None;
+        private static Scene currentScene;
 
         private static bool isAlive = true;
         public static int x = 0, y = 0;
@@ -67,28 +58,16 @@ namespace Dust
         {
             x = e.X;
             y = e.Y;
-
-            if (data[x, y] == PixelType.None && drawState != PixelType.None) data[x, y] = drawState;
         }
 
         static void Event_MouseButtonPressed(object sender, MouseButtonEventArgs e)
         {
-            switch (e.Button)
-            {
-                case Mouse.Button.Left:
-                    drawState = PixelType.Powder;
-                    if (data[x, y] == PixelType.None) data[x, y] = PixelType.Powder;
-                    break;
-                case Mouse.Button.Right:
-                    drawState = PixelType.Block;
-                    if (data[x, y] == PixelType.None) data[x, y] = PixelType.Block;
-                    break;
-            }
+
         }
 
         static void Event_MouseButtonReleased(object sender, MouseButtonEventArgs e)
         {
-            drawState = PixelType.None;
+
         }
     }
 }
